298bb5e29b5a0b9aba4d4f1580bca7cbc7feb9cd
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-09-28 12:52:40 -0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -12,3 +12,16 @@
 # - "that is a right triangle"
 # - "that is an acute triangle"
 # - "that is an obtuse triangle"
+import math
+userip1 = input("Enter number 1 ")
+userip2 = input("Enter number 2 ")
+userip3 = input("Enter number 3 make this number highr than the two before it ")
+userip1 = float(userip1)
+userip2 = float(userip2)
+userip3 = float(userip3)
+if (userip1 * userip1 + userip2 * userip2)==(userip3*userip3):
+    print ("that is a right triangle")
+elif (userip1 * userip1 + userip2 * userip2)>(userip3*userip3):
+    print ("that is an acute triangle")
+else:
+    print ("that is an obtuse triangle")
