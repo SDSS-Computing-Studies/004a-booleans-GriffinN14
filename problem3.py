8b82e4b25fc13c68a056104885487f365d640329
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-01 11:24:13 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,23 +1,11 @@
- #! python3
-
-#  Have the user enter a username 
-# If the username is not "admin" then tell them it is an "invalid user", 
-# but if it is, then ask them for a password 
-# If they get the password correct (password is 12345password) 
-# then display the message "Access granted"
-# 1 marks
 username = ("admin")
 password = ("12345password")
 ug = ("")
 pg = ("")
-while not pg == password:
-    ug = input("Enter username ").strip()
-    if ug == username :
-        pg = input("Enter password ").strip()
-        if pg == password:
-            break
-        else:
-            print ("invalid password")
+if input("Enter user ").strip() == username:
+    if input("Enter password").strip() == password:
+        print ("Access granted")
     else:
-        print ("invalid user")
-print("Access granted")
+        print ("invalid password")
+else:
+    print("invalid user")
